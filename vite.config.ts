7b6a4fbd5e253deea851efbc8987bf72ede66b5d
Yu0u <1713411749@qq.com>
2023-02-24 00:00:23 +0800

    style: add prettier
    添加vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig, loadEnv } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+        cpns: resolve(__dirname, 'src/components'),
+      },
+      extensions: ['.js', '.json', '.ts', '.vue'], // 使用路径别名时想要省略的后缀名，可以自己 增减
+    },
+    /* more config */
+    plugins: [
+      vue({
+        reactivityTransform: true,
+      }),
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_APP_API_BASE_URL,
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  }
 })
